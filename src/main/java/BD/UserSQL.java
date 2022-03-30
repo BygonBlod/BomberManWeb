5b5b2554026ef5c92b4fb07dff876a5ae817b43e
Antonin-BygonBlod <antonin.hunault@yahoo.fr>
2022-03-30 14:27:10 +0200

    ajout du hashage de mot de passe

--- src/main/java/BD/UserSQL.java
+++ src/main/java/BD/UserSQL.java
@@ -1,5 +1,7 @@
 package BD;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -50,6 +52,7 @@ public class UserSQL {
 		PreparedStatement query = null;
 		int resultat = -1;
 		System.out.println("ajout");
+		pwd = hash(pwd);
 		try {
 			query = connexion.prepareStatement("insert into Users (name,password,nbParty,nbWin) values (?,?,?,?) ;");
 			query.setString(1, name);
@@ -115,6 +118,29 @@ public class UserSQL {
 			return false;
 		} else
 			return true;
+	}
+
+	public String hash(String pwd) {
+		String res = "";
+
+		MessageDigest md;
+		try {
+			md = MessageDigest.getInstance("SHA-256");
+			md.update(pwd.getBytes());
+
+			byte byteData[] = md.digest();
+
+			// convertir le tableau de bits en une format hexadécimal - méthode 1
+			StringBuffer sb = new StringBuffer();
+			for (int i = 0; i < byteData.length; i++) {
+				sb.append(Integer.toString((byteData[i] & 0xff) + 0x100, 16).substring(1));
+			}
+			res = sb.toString();
+			System.out.println("En format hexa : " + sb.toString());
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return res;
 	}
 
 }
